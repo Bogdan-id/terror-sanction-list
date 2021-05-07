930e1621fe40bed4cb14617f3fe9c166b0b5df7e
bogdan-t <user332145@gmail.com>
2021-05-07 10:55:27 +0300

    added user & password env variables

--- connector.js
+++ connector.js
@@ -3,7 +3,7 @@ const parser = require('./parser')
 
 const dbName = process.env.DB_NAME;
 const dbCollection = process.env.parseschema;
-const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}`;
+const url = `mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`;
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true
